1348b8b4efd60c2e89fe84dd953c8aa570450ac5
CCEFERDEV <cefer.dev@gmail.com>
2023-02-14 15:42:14 -0400

    nuevoscambios

--- contemplate.js
+++ contemplate.js
@@ -1,5 +1,7 @@
 // primero el template(1) y despues el fragment(2)
 // Modificamos el template (3) hacemos un clon (4)
+// agregar al fragment y luego se agrega a la lista
+
 const lista = document.querySelector('#lista')
 const arrayLista = ['Item 1','Item 2','Item 3']
 //1
