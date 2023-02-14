3c7c9b5f56383ee754e385e055930815ecd0ae1e
CCEFERDEV <cefer.dev@gmail.com>
2023-02-14 15:31:41 -0400

    cambiosUrgentes

---
+++ contemplate.js
@@ -0,0 +1,17 @@
+// primero el template(1) y despues el fragment(2)
+// Modificamos el template (3) hacemos un clon (4)
+const lista = document.querySelector('#lista')
+const arrayLista = ['Item 1','Item 2','Item 3']
+//1
+const template = document.querySelector('#template-li').content
+//2
+const fragment = document.createDocumentFragment()
+
+arrayLista.forEach(item => {
+    template.querySelector('.text-danger').textContent = item
+    const clone = template.cloneNode(true)
+    fragment.appendChild(clone)
+})
+
+lista.appendChild(fragment)
+
